ea74c104d4e877715d9a6ddf8652a5c59f7cb1cd
mani <mani>
2022-12-13 20:59:57 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
+import java.util.concurrent.locks.ReadWriteLock;
+
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly obj=new RWOnly();
+//      obj.name=Mnai;
+//      System.out.print(obj.name);
+//      name has private access in com.driver.RWOnly
+      obj.setName("Mani");
+      System.out.println(obj.getName());
+  }
 }
